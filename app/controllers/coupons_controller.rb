ed806b192aff2f1fa524664654617bce38fa7c78
kelly_maass <kmaass77627@gmail.com>
2020-03-29 21:35:04 -0500

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 
 class CouponsController < ApplicationController
 
@@ -17,8 +17,8 @@ class CouponsController < ApplicationController
 
     def create 
         @coupon = Coupon.new 
-        @coupon.coupon_code = params[:coupon_code]
-        @coupon.store = params[:store]
+        @coupon.coupon_code = params[:coupon][:coupon_code]
+        @coupon.store = params[:coupon][:store]
         @coupon.save 
         redirect_to coupon_path(@coupon)
     end 
